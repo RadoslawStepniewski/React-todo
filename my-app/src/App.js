9a23781722e7a197ca11fb6d930d7c463858536e
radoslawstepniewski <radoslawstepniewski89@gmail.com>
2018-04-27 11:09:56 +0200

    added state

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,12 +3,20 @@ import Person from './components/person';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    person: [
+      {name: "Radek", age: 23},
+      {name: "Tomek", age: 67},
+      {name: "Aga", age: 73}
+    ]
+  }
   render() {
     return (
       <div >
-        <Person name={"Radek"} age={23}/>
-        <Person name={"Tomek"} age={75}/>
-        <Person name={"Aga"} age={45}/>
+        <Person name={this.state.person[0].name} age={this.state.person[0].age}/>
+        <Person name={this.state.person[1].name} age={this.state.person[1].age}/>
+        <Person name={this.state.person[2].name} age={this.state.person[2].age}/>
       </div>
     );
   }
--- my-app/src/components/person.js
+++ my-app/src/components/person.js
@@ -3,7 +3,7 @@ import React, { Components } from 'react';
 const Person = (props) =>{
     return(
         <div>
-            <h1>Masz na imie{props.name} i masz {props.age}</h1>
+            <h1>Masz na imie {props.name} i masz {props.age}</h1>
         </div>
     );
 }
