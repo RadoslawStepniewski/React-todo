f9a6fea927b07cab1ed6323663b222ca4d147425
radoslawstepniewski <radoslawstepniewski89@gmail.com>
2018-04-27 11:16:55 +0200

    added button and setstate

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -11,9 +11,20 @@ class App extends Component {
       {name: "Aga", age: 73}
     ]
   }
+
+  switchNameHandler = ()=> {
+    this.setState({
+      person: [
+        {name: "Radek123", age: 243},
+        {name: "Tomek123", age: 637},
+        {name: "Aga123", age: 723}
+      ]
+    });
+  }
   render() {
     return (
       <div >
+        <button onClick={this.switchNameHandler}>Click</button>
         <Person name={this.state.person[0].name} age={this.state.person[0].age}/>
         <Person name={this.state.person[1].name} age={this.state.person[1].age}/>
         <Person name={this.state.person[2].name} age={this.state.person[2].age}/>
